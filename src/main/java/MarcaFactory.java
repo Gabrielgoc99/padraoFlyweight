4f89909e7cf8ec82596fe62087a5bd8639582ab1
Gabrielgoc99 <gabrielgoc99@gmail.com>
2021-09-30 18:01:35 -0300

    Final Commit

---
+++ src/main/java/MarcaFactory.java
@@ -0,0 +1,22 @@
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MarcaFactory {
+    private static Map<String, Marca> marcas = new HashMap<>();
+
+    public static Marca getMarca(String nome, String nacionalidade) {
+        Marca marca = marcas.get(nome);
+        if (marca == null) {
+            marca = new Marca(nome, nacionalidade);
+            marcas.put(nome, marca);
+        }
+        return marca;
+    }
+
+    public static int getTotalMarcas() {
+        return marcas.size();
+    }
+
+}
